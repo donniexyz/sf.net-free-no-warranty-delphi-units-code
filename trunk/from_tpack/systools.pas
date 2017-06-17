01ac2db5e659ab7a14a0cfe947c7fca3d4df7a5d
hreftoolscorp <hreftoolscorp@7bc16a6e-c703-415e-b6c2-b2145929720d>
2017-06-17 09:14:52 +0000

    example calling an external DLL, here to shutdown Windows NT

---
+++ trunk/from_tpack/systools.pas
@@ -0,0 +1,34 @@
+unit systools;
+
+(*
+Permission is hereby granted, on 11-Apr-2003, free of charge, to any person 
+obtaining a copy of this file (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+THE SOFTWARE.
+*)
+
+interface
+
+function ShutdownNT(ti: integer; force: boolean; rebafter: boolean): integer; cdecl;
+
+implementation
+
+const
+  systls = 'systools.dll';
+
+function ShutdownNT;            external    systls name 'ShutdownNT';
+
+end.
